3e72945455fa909e0ab0ddba8ce952d785b19655
angusyky <angusyiuky@gmail.com>
2024-01-29 13:27:12 -0500

    Implement tests for IntQueue interface

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -14,14 +14,15 @@ import static org.junit.Assert.*;
 
 
 /**
- * TODO: 
+ * TODO:
  * 1. The {@link LinkedIntQueue} has no bugs. We've provided you with some example test cases.
- * Write your own unit tests to test against IntQueue interface with specification testing method 
+ * Write your own unit tests to test against IntQueue interface with specification testing method
  * using mQueue = new LinkedIntQueue();
- * 
- * 2. 
+ * <p>
+ * 2.
  * Comment `mQueue = new LinkedIntQueue();` and uncomment `mQueue = new ArrayIntQueue();`
- * Use your test cases from part 1 to test ArrayIntQueue and find bugs in the {@link ArrayIntQueue} class
+ * Use your test cases from part 1 to test ArrayIntQueue and find bugs in the
+ * {@link ArrayIntQueue} class
  * Write more unit tests to test the implementation of ArrayIntQueue, with structural testing method
  * Aim to achieve 100% line coverage for ArrayIntQueue
  *
@@ -29,81 +30,98 @@ import static org.junit.Assert.*;
  */
 public class IntQueueTest {
 
-    private IntQueue mQueue;
-    private List<Integer> testList;
+  private IntQueue mQueue;
+  private List<Integer> testList;
 
-    /**
-     * Called before each test.
-     */
-    @Before
-    public void setUp() {
-        // comment/uncomment these lines to test each class
-        mQueue = new LinkedIntQueue();
-    //    mQueue = new ArrayIntQueue();
+  /**
+   * Called before each test.
+   */
+  @Before
+  public void setUp() {
+    // comment/uncomment these lines to test each class
+    // mQueue = new LinkedIntQueue();
+    mQueue = new ArrayIntQueue();
 
-        testList = new ArrayList<>(List.of(1, 2, 3));
+    testList = new ArrayList<>(List.of(1, 2, 3));
+  }
+
+  @Test
+  public void testIsEmpty() {
+    // This is an example unit test
+    assertTrue(mQueue.isEmpty());
+  }
+
+  @Test
+  public void testNotEmpty() {
+    final Integer TEST_VALUE = 1;
+    mQueue.enqueue(TEST_VALUE);
+    assertFalse(mQueue.isEmpty());
+  }
+
+  @Test
+  public void testPeekEmptyQueue() {
+    assertNull(mQueue.peek());
+  }
+
+  @Test
+  public void testPeekNoEmptyQueue() {
+    final Integer TEST_VALUE = 1;
+    mQueue.enqueue(TEST_VALUE);
+    assertEquals(mQueue.peek(), TEST_VALUE);
+  }
+
+  @Test
+  public void testEnqueue() {
+    // This is an example unit test
+    for (int i = 0; i < testList.size(); i++) {
+      mQueue.enqueue(testList.get(i));
+      assertEquals(testList.getFirst(), mQueue.peek());
+      assertEquals(i + 1, mQueue.size());
+    }
+  }
+
+  @Test
+  public void testDequeue() {
+    // Setup with enqueue
+    for (int i = 0; i < testList.size(); i++) {
+      mQueue.enqueue(testList.get(i));
+      assertEquals(testList.getFirst(), mQueue.peek());
+      assertEquals(i + 1, mQueue.size());
     }
 
-    @Test
-    public void testIsEmpty() {
-        // This is an example unit test
-        assertTrue(mQueue.isEmpty());
+    // Verify with dequeue
+    for (int i = 0; i < testList.size(); i++) {
+      assertEquals(testList.get(i), mQueue.dequeue());
+      assertEquals(testList.size() - i - 1, mQueue.size());
     }
+  }
 
-    @Test
-    public void testNotEmpty() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+  @Test
+  public void testDequeueEmptyQueue() {
+    assertNull(mQueue.dequeue());
+  }
+
+
+  @Test
+  public void testContent() throws IOException {
+    // This is an example unit test
+    InputStream in = new FileInputStream("src/test/resources/data.txt");
+    try (Scanner scanner = new Scanner(in)) {
+      scanner.useDelimiter("\\s*fish\\s*");
+
+      List<Integer> correctResult = new ArrayList<>();
+      while (scanner.hasNextInt()) {
+        int input = scanner.nextInt();
+        correctResult.add(input);
+        System.out.println("enqueue: " + input);
+        mQueue.enqueue(input);
+      }
+
+      for (Integer result : correctResult) {
+        assertEquals(mQueue.dequeue(), result);
+      }
     }
-
-    @Test
-    public void testPeekEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
-    }
-
-    @Test
-    public void testPeekNoEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
-    }
-
-    @Test
-    public void testEnqueue() {
-        // This is an example unit test
-        for (int i = 0; i < testList.size(); i++) {
-            mQueue.enqueue(testList.get(i));
-            assertEquals(testList.get(0), mQueue.peek());
-            assertEquals(i + 1, mQueue.size());
-        }
-    }
-
-    @Test
-    public void testDequeue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
-    }
-
-    @Test
-    public void testContent() throws IOException {
-        // This is an example unit test
-        InputStream in = new FileInputStream("src/test/resources/data.txt");
-        try (Scanner scanner = new Scanner(in)) {
-            scanner.useDelimiter("\\s*fish\\s*");
-
-            List<Integer> correctResult = new ArrayList<>();
-            while (scanner.hasNextInt()) {
-                int input = scanner.nextInt();
-                correctResult.add(input);
-                System.out.println("enqueue: " + input);
-                mQueue.enqueue(input);
-            }
-
-            for (Integer result : correctResult) {
-                assertEquals(mQueue.dequeue(), result);
-            }
-        }
-    }
+  }
 
 
 }
